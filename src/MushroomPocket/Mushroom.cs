a22176d1bf5d69b245191c57e6ebe709b19c0e52
AlexNg <contact@ngjx.org>
2024-05-02 09:15:48 +0800

    refactor: Move namespace to top

--- src/MushroomPocket/Mushroom.cs
+++ src/MushroomPocket/Mushroom.cs
@@ -1,12 +1,21 @@
-namespace MushroomPocket
-{
-  public class MushroomMaster
-  {
-    public string Name { get; set; }
-    public int NoToTransform { get; set; }
-    public string TransformTo { get; set; }
+namespace MushroomPocket;
 
-    public MushroomMaster(string name, int noToTransform, string transformTo)
+
+public class MushroomMaster
+{
+  public string Name { get; set; }
+  public int NoToTransform { get; set; }
+  public string TransformTo { get; set; }
+
+  public MushroomMaster(string name, int noToTransform, string transformTo)
+  {
+    this.Name = name;
+    this.NoToTransform = noToTransform;
+    this.TransformTo = transformTo;
+  }
+}
+
+
     {
       this.Name = name;
       this.NoToTransform = noToTransform;
