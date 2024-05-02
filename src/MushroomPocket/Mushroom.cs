c9aeac26d74150197ee9b084171b51df6e0993d0
AlexNg <contact@ngjx.org>
2024-05-02 10:17:42 +0800

    style: Use 4 space indents

--- src/MushroomPocket/Mushroom.cs
+++ src/MushroomPocket/Mushroom.cs
@@ -3,151 +3,151 @@ namespace MushroomPocket;
 
 public class MushroomMaster
 {
-  public string Name { get; set; }
-  public int NoToTransform { get; set; }
-  public string TransformTo { get; set; }
+    public string Name { get; set; }
+    public int NoToTransform { get; set; }
+    public string TransformTo { get; set; }
 
-  public MushroomMaster(string name, int noToTransform, string transformTo)
-  {
-    this.Name = name;
-    this.NoToTransform = noToTransform;
-    this.TransformTo = transformTo;
-  }
+    public MushroomMaster(string name, int noToTransform, string transformTo)
+    {
+        this.Name = name;
+        this.NoToTransform = noToTransform;
+        this.TransformTo = transformTo;
+    }
 }
 
 
 [Flags]
 public enum CharacterNames
 {
-  Daisy,
-  Luigi,
-  Mario,
-  Peach,
-  Waluigi,
-  Wario,
+    Daisy,
+    Luigi,
+    Mario,
+    Peach,
+    Waluigi,
+    Wario,
 }
 
 public abstract class Character
 {
-  public int Exp { get; set; }
-  public float Hp { get; set; }
-  public abstract string Name { get; }
-  public abstract string Skill { get; }
-  public abstract bool EvolvedOnly { get; }
+    public int Exp { get; set; }
+    public float Hp { get; set; }
+    public abstract string Name { get; }
+    public abstract string Skill { get; }
+    public abstract bool EvolvedOnly { get; }
 
-  public Character(float hp, int exp)
-  {
-    this.Hp = hp;
-    this.Exp = exp;
-  }
-
-  /// <summary>
-  /// Validate name
-  /// </summary>
-  public static bool isValidName(string? name, out CharacterNames charName) => Enum.TryParse<CharacterNames>(name, out charName);
-
-  /// <summary>
-  /// Get a character from its name
-  ///
-  /// Throws if name is an invalid character
-  /// </summary>
-  public static Character? from(string name, float hp, int exp, out string? errOut, bool noEvolve = true)
-  {
-    CharacterNames characterName;
-    if (!Character.isValidName(name, out characterName))
+    public Character(float hp, int exp)
     {
-      errOut = $"Character name {name} is invalid!";
-      return null;
+        this.Hp = hp;
+        this.Exp = exp;
     }
 
-    return _from(characterName, hp, exp, out errOut, noEvolve);
-  }
+    /// <summary>
+    /// Validate name
+    /// </summary>
+    public static bool isValidName(string? name, out CharacterNames charName) => Enum.TryParse<CharacterNames>(name, out charName);
 
-  /// <summary>
-  /// Get a character from its enum
-  /// </summary>
-  public static Character? from(CharacterNames characterName, float hp, int exp, out string? errOut, bool noEvolve = true)
-  {
-    return _from(characterName, hp, exp, out errOut, noEvolve);
-  }
-
-  private static Character? _from(CharacterNames characterName, float hp, int exp, out string? errOut, bool noEvolve = true)
-  {
-    Character c;
-    switch (characterName)
+    /// <summary>
+    /// Get a character from its name
+    ///
+    /// Throws if name is an invalid character
+    /// </summary>
+    public static Character? from(string name, float hp, int exp, out string? errOut, bool noEvolve = true)
     {
-      case CharacterNames.Daisy:
-        c = new Daisy(hp, exp);
-        break;
-      case CharacterNames.Luigi:
-        c = new Luigi(hp, exp);
-        break;
-      case CharacterNames.Mario:
-        c = new Mario(hp, exp);
-        break;
-      case CharacterNames.Peach:
-        c = new Peach(hp, exp);
-        break;
-      case CharacterNames.Waluigi:
-        c = new Waluigi(hp, exp);
-        break;
-      case CharacterNames.Wario:
-        c = new Wario(hp, exp);
-        break;
-      default:
-        errOut = "Character was not found!";
-        return null;
+        CharacterNames characterName;
+        if (!Character.isValidName(name, out characterName))
+        {
+            errOut = $"Character name {name} is invalid!";
+            return null;
+        }
+
+        return _from(characterName, hp, exp, out errOut, noEvolve);
     }
 
-    if (noEvolve && c.EvolvedOnly)
+    /// <summary>
+    /// Get a character from its enum
+    /// </summary>
+    public static Character? from(CharacterNames characterName, float hp, int exp, out string? errOut, bool noEvolve = true)
     {
-      errOut = $"Character name {c.Name} can only be obtained by evolving!";
-      return null;
+        return _from(characterName, hp, exp, out errOut, noEvolve);
     }
 
-    errOut = null;
-    return c;
-  }
+    private static Character? _from(CharacterNames characterName, float hp, int exp, out string? errOut, bool noEvolve = true)
+    {
+        Character c;
+        switch (characterName)
+        {
+            case CharacterNames.Daisy:
+                c = new Daisy(hp, exp);
+                break;
+            case CharacterNames.Luigi:
+                c = new Luigi(hp, exp);
+                break;
+            case CharacterNames.Mario:
+                c = new Mario(hp, exp);
+                break;
+            case CharacterNames.Peach:
+                c = new Peach(hp, exp);
+                break;
+            case CharacterNames.Waluigi:
+                c = new Waluigi(hp, exp);
+                break;
+            case CharacterNames.Wario:
+                c = new Wario(hp, exp);
+                break;
+            default:
+                errOut = "Character was not found!";
+                return null;
+        }
+
+        if (noEvolve && c.EvolvedOnly)
+        {
+            errOut = $"Character name {c.Name} can only be obtained by evolving!";
+            return null;
+        }
+
+        errOut = null;
+        return c;
+    }
 }
 
 public class Daisy(float hp, int exp) : Character(hp, exp)
 {
-  public override string Name { get; } = "Daisy";
-  public override bool EvolvedOnly { get; } = false;
-  public override string Skill { get; } = "Leadership";
+    public override string Name { get; } = "Daisy";
+    public override bool EvolvedOnly { get; } = false;
+    public override string Skill { get; } = "Leadership";
 }
 
 public class Waluigi(float hp, int exp) : Character(hp, exp)
 {
-  public override string Name { get; } = "Waluigi";
-  public override bool EvolvedOnly { get; } = false;
-  public override string Skill { get; } = "Agility";
+    public override string Name { get; } = "Waluigi";
+    public override bool EvolvedOnly { get; } = false;
+    public override string Skill { get; } = "Agility";
 }
 
 public class Wario(float hp, int exp) : Character(hp, exp)
 {
-  public override string Name { get; } = "Wario";
-  public override bool EvolvedOnly { get; } = false;
-  public override string Skill { get; } = "Strength";
+    public override string Name { get; } = "Wario";
+    public override bool EvolvedOnly { get; } = false;
+    public override string Skill { get; } = "Strength";
 }
 
 public class Luigi(float hp, int exp) : Character(hp, exp)
 {
-  public override string Name { get; } = "Luigi";
-  public override bool EvolvedOnly { get; } = true;
-  public override string Skill { get; } = "Precision and Accuracy";
+    public override string Name { get; } = "Luigi";
+    public override bool EvolvedOnly { get; } = true;
+    public override string Skill { get; } = "Precision and Accuracy";
 }
 
 public class Peach(float hp, int exp) : Character(hp, exp)
 {
-  public override string Name { get; } = "Peach";
-  public override bool EvolvedOnly { get; } = true;
-  public override string Skill { get; } = "Magic Abilities";
+    public override string Name { get; } = "Peach";
+    public override bool EvolvedOnly { get; } = true;
+    public override string Skill { get; } = "Magic Abilities";
 }
 
 public class Mario(float hp, int exp) : Character(hp, exp)
 {
-  public override string Name { get; } = "Mario";
-  public override bool EvolvedOnly { get; } = true;
-  public override string Skill { get; } = "Combat Skills";
+    public override string Name { get; } = "Mario";
+    public override bool EvolvedOnly { get; } = true;
+    public override string Skill { get; } = "Combat Skills";
 }
--- src/MushroomPocket/Program.cs
+++ src/MushroomPocket/Program.cs
@@ -3,121 +3,131 @@ namespace MushroomPocket;
 
 class Program
 {
-  static void Main(string[] args)
-  {
-    if (args.Length > 0)
+    static void Main(string[] args)
     {
-      Console.WriteLine("Usage: MushroomPocket");
-      Environment.Exit(1);
+        if (args.Length > 0)
+        {
+            Console.WriteLine("Usage: MushroomPocket");
+            Environment.Exit(1);
+        }
+
+        // MushroomMaster criteria list for checking character transformation availability.
+        List<MushroomMaster> mushroomMasters = new List<MushroomMaster>(){
+            new MushroomMaster("Daisy", 2, "Peach"),
+            new MushroomMaster("Wario", 3, "Mario"),
+            new MushroomMaster("Waluigi", 1, "Luigi")
+        };
+
+        List<Character> pocket = new List<Character>();
+
+        // Main event loop.
+        while (true)
+        {
+            // Ask for action.
+            Console.Write(String.Join("\n", [
+                @"******************************",
+                @"Welcome to Mushroom Pocket App",
+                @"********************************",
+                @"(1). Add Mushroom's character to my pocket",
+                @"(2). List character(s) in my pocket",
+                @"(3). Check if I can transform my characters",
+                @"(4). Transform my character(s)",
+                @"Please only enter [1, 2, 3, 4] or Q to quit: "
+            ]));
+            string? action = Console.ReadLine();
+
+            switch (action == null ? action : action.ToLower())
+            {
+                case "1":
+                    // Boundary check
+                    if (pocket.Count >= 3)
+                    {
+                        Console.WriteLine("\nI can only hold 3 characters. Please remove some characters first.");
+                        break;
+                    }
+
+                    // Name
+                    Console.Write("Enter Character's Name: ");
+                    string? charName = Console.ReadLine();
+
+                    if (!((string[])["Daisy", "Wario", "Waluigi"]).Contains(charName))
+                    {
+                        Console.WriteLine("\nInvalid character name. Please only enter ['Daisy', 'Wario', 'Waluigi'].");
+                        break;
+                    }
+
+                    // HP
+                    Console.Write("Enter Character's HP: ");
+                    float charHP;
+                    if (!float.TryParse(Console.ReadLine(), out charHP))
+                    {
+                        Console.WriteLine("\nInvalid HP. Please only enter a number.");
+                        break;
+                    }
+
+                    // Exp
+                    Console.Write("Enter Character's EXP: ");
+                    int charEXP;
+                    if (!int.TryParse(Console.ReadLine(), out charEXP))
+                    {
+                        Console.WriteLine("\nInvalid EXP. Please only enter an integer.");
+                        break;
+                    }
+
+                    // Add char
+                    // I'm too used to Golang lol
+                    // if newChar, err := Character.from(charName, charHP, charEXP); err != nil {
+                    //   // Error
+                    // }
+                    // :>
+                    string? errOut;
+                    Character? newChar = Character.from(charName!, charHP, charEXP, out errOut);
+                    if (errOut != null)
+                    {
+                        Console.WriteLine("\n" + errOut);
+                        break;
+                    }
+
+                    pocket.Add(newChar!);
+                    Console.WriteLine($"{charName} has been added.");
+                    break;
+
+                case "2":
+                    // Sort descending
+                    pocket.Sort((Character c1, Character c2) => c2.Hp.CompareTo(c1.Hp));
+                    foreach (Character c in pocket)
+                    {
+                        Console.WriteLine(String.Join("\n",
+                            @"-----------------------",
+                            $"Name: {c.Name}",
+                            $"HP: {c.Hp}",
+                            $"EXP: {c.Exp}",
+                            $"Skill: {c.Skill}",
+                            @"-----------------------"
+                        ));
+                    }
+                    break;
+
+                case "3":
+                    // TODO: Check transformation
+                    break;
+
+                case "4":
+                    // TODO: Transform character
+                    break;
+
+                case "q" or null:
+                    Console.WriteLine("Thanks for playing. Good bye!");
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("\nInvalid action. Please only enter [1, 2, 3, 4] or Q to quit.");
+                    break;
+            }
+
+            // Insert newline
+            Console.WriteLine();
+        }
     }
-
-    // MushroomMaster criteria list for checking character transformation availability.
-    List<MushroomMaster> mushroomMasters = new List<MushroomMaster>(){
-        new MushroomMaster("Daisy", 2, "Peach"),
-        new MushroomMaster("Wario", 3, "Mario"),
-        new MushroomMaster("Waluigi", 1, "Luigi")
-      };
-
-    List<Character> pocket = new List<Character>();
-
-    // Main event loop.
-    while (true)
-    {
-      // Ask for action.
-      Console.Write(String.Join("\n", [
-        @"******************************",
-        @"Welcome to Mushroom Pocket App",
-        @"********************************",
-        @"(1). Add Mushroom's character to my pocket",
-        @"(2). List character(s) in my pocket",
-        @"(3). Check if I can transform my characters",
-        @"(4). Transform my character(s)",
-        @"Please only enter [1, 2, 3, 4] or Q to quit: "
-      ]));
-      string? action = Console.ReadLine();
-
-      switch (action == null ? action : action.ToLower())
-      {
-        case "1":
-          // Boundary check
-          if (pocket.Count >= 3)
-          {
-            Console.WriteLine("I can only hold 3 characters. Please remove some characters first.\n");
-            break;
-          }
-
-          // Name
-          Console.Write("Enter Character's Name: ");
-          string? charName = Console.ReadLine();
-
-          if (!((string[])["Daisy", "Wario", "Waluigi"]).Contains(charName))
-          {
-            Console.WriteLine("Invalid character name. Please only enter ['Daisy', 'Wario', 'Waluigi'].\n");
-            break;
-          }
-
-          // HP
-          Console.Write("Enter Character's HP: ");
-          float charHP;
-          if (!float.TryParse(Console.ReadLine(), out charHP))
-          {
-            Console.WriteLine("Invalid HP. Please only enter a number.\n");
-            break;
-          }
-
-          // Exp
-          Console.Write("Enter Character's EXP: ");
-          int charEXP;
-          if (!int.TryParse(Console.ReadLine(), out charEXP))
-          {
-            Console.WriteLine("Invalid EXP. Please only enter an integer.\n");
-            break;
-          }
-
-          // Add char
-          // I'm too used to Golang lol
-          // if newChar, err := Character.from(charName, charHP, charEXP); err != nil {
-          //   // Error
-          // }
-          // :>
-          string? errOut;
-          Character? newChar = Character.from(charName!, charHP, charEXP, out errOut);
-          if (errOut != null)
-          {
-            Console.WriteLine(errOut + "\n");
-            break;
-          }
-
-          pocket.Add(newChar!);
-          Console.WriteLine($"{charName} has been added.\n");
-          break;
-
-        case "2":
-          // Sort descending
-          pocket.Sort((Character c1, Character c2) => c2.Hp.CompareTo(c1.Hp));
-          foreach (Character character in pocket)
-          {
-            Console.WriteLine(character.ToString());
-          }
-          break;
-
-        case "3":
-          // TODO: Check transformation
-          break;
-
-        case "4":
-          // TODO: Transform character
-          break;
-
-        case "q" or null:
-          Console.WriteLine("Thanks for playing. Good bye!");
-          Environment.Exit(0);
-          break;
-
-        default:
-          Console.WriteLine("Invalid action. Please only enter [1, 2, 3, 4] or Q to quit.\n");
-          break;
-      }
-    }
-  }
 }
