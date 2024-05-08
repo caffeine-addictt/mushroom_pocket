f6f6993b4f999948474a57302d110733504785b4
AlexNg <contact@ngjx.org>
2024-05-08 21:50:21 +0800

    refactor: Abstract to Core

---
+++ src/MushroomPocket/Core/ManageTeams.cs
@@ -0,0 +1,338 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Ng Jun Xiang <contact@ngjx.org>
+ * SPDX-License-Identifier: GPL-3.0-only
+ *
+ * Name: Ng Jun Xiang
+ * Admin: 230725N
+ */
+
+using Microsoft.EntityFrameworkCore;
+using MushroomPocket.Models;
+using MushroomPocket.Utils;
+
+namespace MushroomPocket.Core;
+
+
+public static class ManageTeams
+{
+    public static readonly string InterfaceText = String.Join(
+        "\n",
+        [
+            @"",
+            @"(1). Add a new team to my pocket",
+            @"(2). Add Mushroom's character to a team",
+            @"(3). List team(s) in my pocket",
+            @"(4). List character(s) in a team",
+            @"(5). Delete team(s) from my pocket",
+            @"Please only enter [1, 2, 3, 4, 5] or b to go back: "
+        ]
+    );
+
+
+    // Main entry point
+    public static void MainEntry()
+    {
+        Console.Write(InterfaceText);
+        switch ((Console.ReadLine() ?? "").ToLower())
+        {
+            case "1":
+                AddTeam();
+                break;
+
+            case "2":
+                AddCharacterToTeam();
+                break;
+
+            case "3":
+                ListTeams();
+                break;
+
+            case "4":
+                ListCharactersInTeam();
+                break;
+
+            case "5":
+                DeleteTeams();
+                break;
+
+            case "b":
+                break;
+
+            default:
+                Console.WriteLine(
+                    "\nInvalid action. Please only enter [1, 2, 3, 4, 5] or Q to quit."
+                );
+                break;
+        }
+    }
+
+    // Option 6-1: Add new team
+    private static void AddTeam()
+    {
+        // Name
+        Console.Write("Enter new team name: ");
+        string teamName = (Console.ReadLine() ?? "").Trim();
+        if (String.IsNullOrWhiteSpace(teamName))
+        {
+            Console.WriteLine("\nTeam name cannot be empty.");
+            return;
+        }
+
+        // Desc
+        Console.Write("Enter a team description: ");
+        string teamDesc = (Console.ReadLine() ?? "").Trim();
+
+        using (MushroomContext db = new MushroomContext())
+        {
+            if (db.Teams.Where((Team t) => t.Name == teamName).Count() > 0)
+            {
+                Console.WriteLine("\nTeam name already exists.");
+                return;
+            }
+            db.Teams.Add(new Team(teamName, teamDesc));
+            db.SaveChanges();
+        }
+
+        Console.WriteLine($"{teamName} has been added.");
+    }
+
+    // Option 6-2: Add character to team
+    private static void AddCharacterToTeam()
+    {
+        // Ask for pattern
+        Console.Write("Enter Character Name or ID [* for all]: ");
+        string namePattern = Console.ReadLine() ?? "";
+
+        if (String.IsNullOrWhiteSpace(namePattern))
+        {
+            Console.WriteLine("\nCharacter name or ID cannot be empty.");
+            return;
+        }
+
+        // Ask for team pattern
+        Console.Write("Enter Team Name or ID [* for all]: ");
+        string teamPattern = Console.ReadLine() ?? "";
+        if (String.IsNullOrWhiteSpace(teamPattern))
+        {
+            Console.WriteLine("\nTeam name or ID cannot be empty.");
+            return;
+        }
+
+        using (MushroomContext db = new MushroomContext())
+        {
+            List<Character> charList =
+                namePattern == "*"
+                    ? db.Characters.ToList()
+                    : db
+                        .Characters.Where(
+                            (Character c) =>
+                                c.Name.StartsWith(namePattern) || c.Id.StartsWith(namePattern)
+                        )
+                        .ToList();
+
+            HashSet<Team> teamList =
+                teamPattern == "*"
+                    ? db.Teams.Include(t => t.Characters).ToHashSet()
+                    : db
+                        .Teams.Include(t => t.Characters)
+                        .Where(
+                            (Team t) =>
+                                t.Name.StartsWith(teamPattern) || t.Id.StartsWith(teamPattern)
+                        )
+                        .ToHashSet();
+
+            // Check if empty
+            if (charList.Count == 0)
+            {
+                Console.WriteLine("\nNo character(s) found. Nothing to add to team.");
+                return;
+            }
+            if (teamList.Count == 0)
+            {
+                Console.WriteLine("\nNo team(s) found. Nothing to add to team.");
+                return;
+            }
+
+            // Confirmation
+            Console.Write(
+                $"Are you sure you want to add {charList.Count} character(s) to {teamList.Count} team(s)? [Y/N]: "
+            );
+            if ((Console.ReadLine() ?? "").ToLower() != "y")
+                return;
+
+            // Add
+            bool hasChange = false;
+            foreach (Team t in teamList)
+            {
+                if (t.Characters.Intersect(t.Characters).Count() > 0)
+                {
+                    if (teamPattern == "*")
+                        continue;
+                    Console.WriteLine(
+                        $"\nTeam {t.Name} already has the character(s) {String.Join(", ", t.Characters)}."
+                    );
+                    return;
+                }
+
+                hasChange = true;
+                t.AddCharacterRange(charList);
+            }
+
+            if (!hasChange)
+            {
+                Console.WriteLine(
+                    $"\nCharacter(s) {String.Join(", ", charList)} already exist in team(s)."
+                );
+                return;
+            }
+
+            db.SaveChanges();
+            Console.WriteLine(
+                $"{charList.Count} character(s) have been added to {teamList.Count} team(s)."
+            );
+        }
+    }
+
+    // Option 6-3: List team(s)
+    private static void ListTeams()
+    {
+        // Sort descending
+        List<Team> sorted;
+        using (MushroomContext db = new MushroomContext())
+        {
+            sorted = db.Teams.Include(t => t.Characters).ToList();
+        }
+        sorted.Sort((Team t1, Team t2) => t2.Characters.Count.CompareTo(t1.Characters.Count));
+
+        if (sorted.Count == 0)
+        {
+            Console.WriteLine("\nNo team(s) to list!");
+            return;
+        }
+
+        foreach (Team t in sorted)
+        {
+            Console.WriteLine(
+                String.Join(
+                    "\n",
+                    @"-----------------------",
+                    $"ID: {t.Id}",
+                    $"Name: {t.Name}",
+                    $"Description: {t.Description}",
+                    $"Character(s): {t.Characters.Count}",
+                    @"-----------------------"
+                )
+            );
+        }
+    }
+
+    // Option 6-4: List character(s) in team
+    private static void ListCharactersInTeam()
+    {
+        // Ask for pattern
+        Console.Write("Enter Team Name or ID: ");
+        string teamPattern = Console.ReadLine() ?? "";
+
+        using (MushroomContext db = new MushroomContext())
+        {
+            Similarity topSuggestion;
+            if (
+                !StringUtils.SmartLookUp(
+                    teamPattern,
+                    db.Teams.Select((Team t) => t.Name).ToList(),
+                    out topSuggestion!
+                )
+            )
+            {
+                Console.WriteLine("\nTeam name or ID not found.");
+                return;
+            }
+
+            // Confirmation if not the same
+            if (
+                StringUtils.Clean(teamPattern, true)
+                != StringUtils.Clean(topSuggestion.QualifiedText, true)
+            )
+            {
+                Console.Write($"\nDid you mean {topSuggestion.QualifiedText}? [Y/N]: ");
+                if ((Console.ReadLine() ?? "").ToLower() != "y")
+                    return;
+            }
+
+            // Stdout characters
+            List<Character> charList = db
+                .Teams.Include(t => t.Characters)
+                .Where((Team t) => t.Name == topSuggestion.QualifiedText)
+                .First()!
+                .Characters.ToList();
+            charList.Sort((Character c1, Character c2) => c2.Hp.CompareTo(c1.Hp));
+
+            if (charList.Count == 0)
+            {
+                Console.WriteLine("\nNo character(s) to list!");
+                return;
+            }
+
+            foreach (Character c in charList)
+            {
+                Console.WriteLine(
+                    String.Join(
+                        "\n",
+                        [
+                            @"-----------------------",
+                            $"ID: {c.Id}",
+                            $"Name: {c.Name}",
+                            $"HP: {c.Hp}",
+                            $"EXP: {c.Exp}",
+                            $"Skill: {c.Skill}",
+                            @"-----------------------"
+                        ]
+                    )
+                );
+            }
+        }
+    }
+
+    // Option 6-5: Delete team(s)
+    private static void DeleteTeams()
+    {
+        // Ask for pattern
+        Console.Write("Enter Team Name or ID [* for all]: ");
+        string? delPattern = Console.ReadLine();
+
+        if (String.IsNullOrWhiteSpace(delPattern))
+        {
+            Console.WriteLine("\nTeam name or ID cannot be empty.");
+            return;
+        }
+
+        using (MushroomContext db = new MushroomContext())
+        {
+            List<Team> delList =
+                (delPattern! == "*")
+                    ? db.Teams.ToList()
+                    : db
+                        .Teams.Where(
+                            (Team t) =>
+                                t.Name.StartsWith(delPattern!) || t.Id.StartsWith(delPattern!)
+                        )
+                        .ToList();
+
+            // Check if empty
+            if (delList.Count == 0)
+            {
+                Console.WriteLine("\nNo team(s) found. Nothing to delete.");
+                return;
+            }
+
+            // Ask for confirmation
+            Console.Write($"Are you sure you want to delete {delList.Count} team(s)? [Y/N]: ");
+            if ((Console.ReadLine() ?? "").ToLower() != "y")
+                return;
+
+            db.RemoveRange(delList);
+            db.SaveChanges();
+            Console.WriteLine($"{delList.Count} team(s) have been deleted.");
+        }
+    }
+}
---
+++ src/MushroomPocket/Core/ManagingCharacters.cs
@@ -0,0 +1,184 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Ng Jun Xiang <contact@ngjx.org>
+ * SPDX-License-Identifier: GPL-3.0-only
+ *
+ * Name: Ng Jun Xiang
+ * Admin: 230725N
+ */
+
+using MushroomPocket.Models;
+using MushroomPocket.Utils;
+
+namespace MushroomPocket.Core;
+
+
+public static class ManageCharacters
+{
+    // Option 1: Add character
+    public static void AddCharacter()
+    {
+        // Name
+        Console.Write("Enter Character's Name: ");
+        Similarity? topSuggestion;
+        List<string> possibleNames = new List<string>() { "Daisy", "Wario", "Waluigi" };
+
+        if (
+            !StringUtils.SmartLookUp(Console.ReadLine() ?? "", possibleNames, out topSuggestion)
+            || topSuggestion == null
+        )
+        {
+            Console.WriteLine(
+                "\nInvalid character name. Please only enter ['Daisy', 'Wario', 'Waluigi']."
+            );
+            return;
+        }
+
+        if (topSuggestion.QualifiedText.ToLower() != topSuggestion.OriginalText.ToLower())
+        {
+            Console.Write(
+                $"\nDid you mean '{topSuggestion.QualifiedText}'? ({topSuggestion.ScoreToString()}%) [Y/N]: "
+            );
+            if ((Console.ReadLine() ?? "").ToLower() != "y")
+                return;
+        }
+
+        // HP
+        Console.Write("Enter Character's HP: ");
+        float charHP;
+        if (!float.TryParse(Console.ReadLine(), out charHP))
+        {
+            Console.WriteLine("\nInvalid HP. Please only enter a number.");
+            return;
+        }
+
+        // Exp
+        Console.Write("Enter Character's EXP: ");
+        int charEXP;
+        if (!int.TryParse(Console.ReadLine(), out charEXP))
+        {
+            Console.WriteLine("\nInvalid EXP. Please only enter an integer.");
+            return;
+        }
+
+        // Add char
+        string? errOut;
+        Character? newChar = Character.From(
+            topSuggestion.QualifiedText,
+            charHP,
+            charEXP,
+            out errOut
+        );
+        if (errOut != null)
+        {
+            Console.WriteLine("\n" + errOut);
+            return;
+        }
+
+        using (MushroomContext db = new MushroomContext())
+        {
+            db.Characters.Add(newChar!);
+            db.SaveChanges();
+        }
+        Console.WriteLine($"{topSuggestion.QualifiedText} has been added.");
+        return;
+    }
+
+    // Option 2: List Characters
+    public static void ListCharacters()
+    {
+        // Sort descending
+        List<Character> sorted;
+        using (MushroomContext db = new MushroomContext())
+        {
+            sorted = db.Characters.OrderByDescending((Character c) => c.Hp).ToList();
+        }
+
+        if (sorted.Count == 0)
+        {
+            Console.WriteLine("\nNo character(s) to list!");
+            return;
+        }
+
+        foreach (Character c in sorted)
+        {
+            Console.WriteLine(
+                String.Join(
+                    "\n",
+                    @"-----------------------",
+                    $"ID: {c.Id}",
+                    $"Name: {c.Name}",
+                    $"HP: {c.Hp}",
+                    $"EXP: {c.Exp}",
+                    $"Skill: {c.Skill}",
+                    @"-----------------------"
+                )
+            );
+        }
+    }
+
+    // Option 3: Check transformation
+    public static void CheckTransformation(List<MushroomMaster> mushroomMasters)
+    {
+        // Check transformation
+        List<MushroomMaster> canEvoList = Character.CanEvolve(mushroomMasters);
+        foreach (MushroomMaster m in canEvoList)
+        {
+            Console.WriteLine($"{m.Name} -> {m.TransformTo}");
+        }
+    }
+
+    // Option 4: Transform characters
+    public static void TransformCharacters(List<MushroomMaster> mushroomMasters)
+    {
+        // Transform character
+        List<MushroomMaster> evolved = Character.Evolve(mushroomMasters);
+        foreach (MushroomMaster m in evolved)
+        {
+            Console.WriteLine($"{m.Name} has been transformed to {m.TransformTo}.");
+        }
+    }
+
+    // Option 5: Delete characters
+    public static void DeleteCharacters()
+
+    {
+        // Ask for pattern
+        Console.Write("Enter Character Name or ID [* for all]: ");
+        string? delPattern = Console.ReadLine();
+
+        if (String.IsNullOrWhiteSpace(delPattern))
+        {
+            Console.WriteLine("\nCharacter name or ID cannot be empty.");
+            return;
+        }
+
+        using (MushroomContext db = new MushroomContext())
+        {
+            List<Character> delList =
+                (delPattern! == "*")
+                    ? db.Characters.ToList()
+                    : db
+                        .Characters.Where(
+                            (Character c) =>
+                                c.Name.StartsWith(delPattern!) || c.Id.StartsWith(delPattern!)
+                        )
+                        .ToList();
+
+            // Check if empty
+            if (delList.Count == 0)
+            {
+                Console.WriteLine("\nNo character(s) found. Nothing to delete.");
+                return;
+            }
+
+            // Ask for confirmation
+            Console.Write($"Are you sure you want to delete {delList.Count} character(s)? [Y/N]: ");
+            if ((Console.ReadLine() ?? "").ToLower() != "y")
+                return;
+
+            db.RemoveRange(delList);
+            db.SaveChanges();
+            Console.WriteLine($"{delList.Count} character(s) have been deleted.");
+        }
+    }
+}
