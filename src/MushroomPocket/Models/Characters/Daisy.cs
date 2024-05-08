fd76f07306cfef448c0f631b17c400ef02d585e0
AlexNg <contact@ngjx.org>
2024-05-08 16:59:23 +0800

    fix: Neovim on windows do be weird af

--- src/MushroomPocket/Models/Characters/Daisy.cs
+++ src/MushroomPocket/Models/Characters/Daisy.cs
@@ -10,6 +10,7 @@ namespace MushroomPocket.Models;
 
 public class Daisy : Character
 {
+    public Daisy(float hp, int exp) : base(hp, exp)
     {
         Name = "Daisy";
         EvolvedOnly = false;
