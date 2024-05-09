c6383b1b01808e32889287b8cb988cacf929b7e8
I-like-coffee <taboez4life@gmail.com>
2024-05-09 14:10:51 +0800

    wip: base

---
+++ src/MushroomPocket/Core/ManageEconomy.cs
@@ -0,0 +1,15 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Ng Jun Xiang <contact@ngjx.org>
+ * SPDX-License-Identifier: GPL-3.0-only
+ *
+ * Name: Ng Jun Xiang
+ * Admin: 230725N
+ */
+
+namespace MushroomPocket.Core;
+
+
+public static class ManageEconomy
+{
+    
+}
---
+++ src/MushroomPocket/Models/Items/Items.cs
@@ -0,0 +1,16 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Ng Jun Xiang <contact@ngjx.org>
+ * SPDX-License-Identifier: GPL-3.0-only
+ *
+ * Name: Ng Jun Xiang
+ * Admin: 230725N
+ */
+
+namespace MushroomPocket.Models;
+
+
+public class Item
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
