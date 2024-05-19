0fdf9c15720c16a48bee95ae93826d96071a1e16
AlexNg <contact@ngjx.org>
2024-05-19 14:14:41 +0800

    feat: Add terminal dimensions

--- src/MushroomPocket/Core/Constants.cs
+++ src/MushroomPocket/Core/Constants.cs
@@ -9,7 +9,26 @@
 namespace MushroomPocket.Core;
 
 
+public struct Dimension
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public int Width => X;
+    public int Height => Y;
+
+    public Dimension(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
+
 public static class Constants
 {
     public static string? CurrentProfileId { get; set; }
+
+    // Terminal
+    public static readonly Dimension TerminalSize = new Dimension(Console.WindowWidth, Console.WindowHeight);
 }
