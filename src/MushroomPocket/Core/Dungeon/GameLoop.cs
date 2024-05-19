ddf035157233b353f4cf75a6e40477b8f456c892
AlexNg <contact@ngjx.org>
2024-05-20 07:34:54 +0800

    feat: Add helper function

--- src/MushroomPocket/Core/Dungeon/GameLoop.cs
+++ src/MushroomPocket/Core/Dungeon/GameLoop.cs
@@ -27,8 +27,13 @@ public static class GameLogic
         Thread.Sleep(3000);
         initialLoadHandler.Dispose();
 
+        // Helper functions
+        bool ContinueGame() => !team.Characters.All(c => c.Hp == 0) && dm.Hp != 0;
+        Character PickCharacter() => team.Characters.ToList()[new Random().Next(0, team.Characters.Count)];
+        void DamageCharacter(Character character, float damage) => character.Hp -= damage;
+
         // Game loop
-        while (!team.Characters.All(c => c.Hp == 0) && dm.Hp != 0)
+        while (ContinueGame())
         {
             // DM moves first
             Frame.DrawFrame(team, dm);
@@ -43,4 +48,10 @@ public static class GameLogic
 
         // Treat as all dead
     }
+
+
+    private static float RollDamage(Character c)
+        => c.Atk * (c.CritRate * 10 < new Random().Next(11) ? 1 : c.CritMultiplier);
+    private static float RollDamage(DungeonMaster dm)
+        => dm.Atk * (new Random().Next(11) / 10);
 }
