550e5f60bc42928d80fa668ddac947d70ea04c5f
AlexNg <contact@ngjx.org>
2024-05-20 00:11:27 +0800

    feat: Add drawing

---
+++ src/MushroomPocket/Core/Dungeon/Draw.cs
@@ -0,0 +1,93 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Ng Jun Xiang <contact@ngjx.org>
+ * SPDX-License-Identifier: GPL-3.0-only
+ *
+ * Name: Ng Jun Xiang
+ * Admin: 230725N
+ */
+
+/* using System.Threading; */
+using MushroomPocket.Models;
+/* using MushroomPocket.Utils; */
+
+namespace MushroomPocket.Core.DungeonGameLogic;
+
+
+public static class Frame
+{
+    public static Dimension ResolveDimension()
+        => new Dimension(ASCIIArt.DungeonMasterDimensions.X * 2, ASCIIArt.DungeonMasterDimensions.Y * 2 + 4);
+
+
+    public static string GenerateHpIndicator(Dimension xy, float hp, float maxHp)
+    {
+        int barWidth = (int)Math.Floor((decimal)ASCIIArt.DungeonMasterDimensions.X / 2);
+
+        return String.Join(
+            "\n",
+            CenterAlign($"Hp: {hp}/{maxHp}", barWidth),
+            $"| {String.Join("", Enumerable.Repeat("#", barWidth - 4))} |"
+        );
+    }
+
+
+    public static string CenterAlign(string s, int width)
+    {
+        List<string> formatted = new List<string>();
+
+        foreach (string line in s.Split("\n"))
+        {
+            int leading = (int)Math.Floor((decimal)(width - line.Length) / 2);
+            leading = Math.Clamp(leading, 0, width);
+
+            formatted.Add(
+                String.Join("", Enumerable.Repeat(" ", leading))
+                + line
+                + String.Join("", Enumerable.Repeat(" ", width - line.Length - leading))
+            );
+        }
+
+        return String.Join("\n", formatted);
+    }
+
+
+    public static void DrawFrame(Team team, DungeonMaster dm)
+    {
+        Console.Clear();
+        Dimension canvas = ResolveDimension();
+
+        string frame = "";
+
+        // Build dungeon DungeonMaster
+        frame += CenterAlign(ASCIIArt.DungeonMaster, canvas.X) + "\n";
+        frame += CenterAlign(GenerateHpIndicator(ASCIIArt.DungeonMasterDimensions, dm.Hp, dm.MaxHp), canvas.X) + "\n\n\n";
+
+        // Build team
+        int teamSpacing = 2;
+        List<List<string>> teamMembers = new List<List<string>>();
+        Dimension teamASCIIDimension = new Dimension(ASCIIArt.CharacterDimensions.X * 2, ASCIIArt.CharacterDimensions.Y);
+
+        foreach (Character character in team.Characters)
+        {
+            string ascii = String.Join(
+                "\n",
+                CenterAlign(ASCIIArt.Character, teamASCIIDimension.X),
+                CenterAlign(GenerateHpIndicator(teamASCIIDimension, character.Hp, character.MaxHp), teamASCIIDimension.X)
+            );
+            teamMembers.Add(ascii.Split("\n").ToList());
+        }
+
+        // Join
+        List<string> formattedLines = new List<string>();
+        for (int i = 0; i < teamMembers[0].Count; i++)
+        {
+            List<string> lineFragment = teamMembers.Select(x => x[i]).ToList();
+            formattedLines.Add(String.Join(String.Join("", Enumerable.Repeat(" ", teamSpacing)), lineFragment));
+        }
+
+        frame += String.Join("\n", formattedLines);
+
+        // Pad Y-Axis
+        Console.WriteLine($"\n\n{frame}\n\n");
+    }
+}
--- src/MushroomPocket/Core/Dungeon/GameLoop.cs
+++ src/MushroomPocket/Core/Dungeon/GameLoop.cs
@@ -15,13 +15,32 @@ namespace MushroomPocket.Core.DungeonGameLogic;
 
 public static class GameLogic
 {
-
     public static void Start(Team team, Dungeon dungeon)
     {
+        // Threaded so I can setup some stuff while it shows loading screen in the meantime
         LoadingHandler initialLoadHandler = Loading.Start();
         DungeonMaster dm = new DungeonMaster(dungeon);
 
+        /* int totalDamageDone; */
+        /* int totalDamageTaken; */
+
         Thread.Sleep(3000);
         initialLoadHandler.Dispose();
+
+        // Game loop
+        while (!team.Characters.All(c => c.Hp == 0) && dm.Hp != 0)
+        {
+            // DM moves first
+            Frame.DrawFrame(team, dm);
+            Thread.Sleep(1000);
+        }
+
+        // Treat as defeated
+        if (dm.Hp == 0)
+        {
+            return;
+        }
+
+        // Treat as all dead
     }
 }
