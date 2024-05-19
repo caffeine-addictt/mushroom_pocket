4c401a2b7a41e628e756327f58d6799adce5f2f9
AlexNg <contact@ngjx.org>
2024-05-20 06:56:56 +0800

    fix: Use leading instead of possibly negative width - line.Length - leading

--- src/MushroomPocket/Core/Dungeon/Draw.cs
+++ src/MushroomPocket/Core/Dungeon/Draw.cs
@@ -43,7 +43,7 @@ public static class Frame
             formatted.Add(
                 String.Join("", Enumerable.Repeat(" ", leading))
                 + line
-                + String.Join("", Enumerable.Repeat(" ", width - line.Length - leading))
+                + String.Join("", Enumerable.Repeat(" ", leading))
             );
         }
 
